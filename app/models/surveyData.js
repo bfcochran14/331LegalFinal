ffd585c8a3460838d5fb61a919516350d66fad66
Ben Cochran <BenCochran@Bens-MacBook-Pro.local>
2018-04-17 17:40:49 -0400

    First Commit, Login complete

---
+++ app/models/surveyData.js
@@ -0,0 +1,5 @@
+var mongoose = require('mongoose');
+var survey_data = mongoose.Schema({
+any: {} });
+
+module.exports = mongoose.model('Data', survey_data);
---
+++ server.js
@@ -0,0 +1,47 @@
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+//connect to database
+var mongoDB='mongodb://ben:admin@ds237979.mlab.com:37979/ndadocument'
+var promise = mongoose.connect(mongoDB, {
+  useMongoClient: true,
+});
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+
+
+// configuration ===============================================================
+
+
+ require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
